b74ec83c901f8e3e73f7de3a97cdfb324f78803a
Peyton Seigo <pseigo@yahoo.com>
2020-07-21 01:22:43 -0700

    Add GNU AGPLv3 license

--- src/socket_courier_cli/main.cpp
+++ src/socket_courier_cli/main.cpp
@@ -1,25 +1,73 @@
 /*
- *  main.cpp
- *  Author: Benjamin Sergeant
- *  Copyright (c) 2020 Machine Zone, Inc. All rights reserved.
+ * Socket Courier
+ * Copyright (C) 2020 Peyton Seigo
  *
- *  Super simple standalone example. See ws folder, unittest and doc/usage.md for more.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  On macOS
- *  $ mkdir -p build ; cd build ; cmake -DUSE_TLS=1 .. ; make -j ; make install
- *  $ clang++ --std=c++14 --stdlib=libc++ main.cpp -lixwebsocket -lz -framework Security -framework Foundation
- *  $ ./a.out
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
-
-#include <iostream>
 
 #include "connection.hpp"
 
-using ws_client::Connection;
+#include <ixwebsocket/IXNetSystem.h>
+#include <ixwebsocket/IXWebSocket.h>
+
+#include <iostream>
 
 int main()
 {
-    Connection c;
+    ws_client::Connection c;
     c.hello_world();
+
+#ifdef _WIN32
+    // Only needed once
+    ix:initNetSystem();
+#endif
+
+    ix::WebSocket ws;
+
+    std::string url("wss://echo.websocket.org");
+    ws.setUrl(url);
+
+    ws.setPingInterval(45); // seconds
+
+    ws.disablePerMessageDeflate();
+
+    ws.setOnMessageCallback([url](const ix::WebSocketMessagePtr &msg) {
+            if (msg->type == ix::WebSocketMessageType::Message) {
+                std::cout << "From '" << url << "': " << msg->str << std::endl;
+            }
+        }
+    );
+
+    ws.start();
+
+    ws.sendBinary("some serialized binary data!");
+
+    while (true) {
+        std::string line;
+        std::cout << "you: " << std::flush;
+        std::getline(std::cin, line);
+
+        if (!std::cin) {
+            break;
+        }
+
+        ws.sendText(line);
+    }
+
+    ws.stop();
+
+#ifdef _WIN32
+    ix:uninitNetSystem();
+#endif
 }
