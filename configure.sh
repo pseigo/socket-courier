8eb93ceb3cbfa5585d04c6de05295fcec009d3f9
Peyton Seigo <pseigo@yahoo.com>
2020-08-20 11:51:12 -0700

    Use clang++

--- configure.sh
+++ configure.sh
@@ -1,1 +1,7 @@
-cmake -B bin -DCMAKE_BUILD_TYPE=DEBUG -DUSE_TLS=1 
+# Use clang if possible 
+if command -v clang &> /dev/null; then
+  echo "Detected clang on this system, setting CXX compiler to clang++"
+  cmake -B bin -DCMAKE_BUILD_TYPE=DEBUG -DUSE_TLS=1 -DCMAKE_CXX_COMPILER=clang++
+else
+  cmake -B bin -DCMAKE_BUILD_TYPE=DEBUG -DUSE_TLS=1 
+fi
